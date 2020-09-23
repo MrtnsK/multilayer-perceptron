07d99ae106a5017f27a4d696f52c3662e025a4c9
Kevin <kemartin@student.42.fr>
2020-09-23 18:08:23 +0200

    bruh

--- mlp_learning.py
+++ mlp_learning.py
@@ -3,18 +3,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sys
 
-def LR_descent_gradient(X, Y):
-	Lrate = 1
-	thetas = np.array([0,0])
-	m = float(len(X))
-
-	while (1):
-		tmp = thetas
-		thetas = thetas - Lrate * (1 / m) * (X.T @ ((X @ thetas) - Y))
-		if np.array_equal(thetas, tmp):
-			break
-	return thetas
-
 if __name__ == "__main__":
 	if (len(sys.argv) != 2):
 		print("Give a valid data.csv")
